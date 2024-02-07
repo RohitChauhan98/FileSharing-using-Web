74a3cac374d5bae2c5aadc56c0151ab29ccd717e
Rohit Chauhan <rc56951@gmail.com>
2024-02-07 20:48:20 +0530

    Rough landing page

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,23 @@
-import { useState } from 'react'
+import './App.css'
 
 function App() {
 
   return (
     <>
-      <div>
-
+    <div>
+      <div className="apptitle">
+        <h1 className="appName">File Share</h1>
+        <img className='imgs' src="https://img4.daumcdn.net/thumb/R658x0.q70/?fname=https://t1.daumcdn.net/news/202105/25/techplus/20210525064051842ngfu.png" alt="error" />
       </div>
+      
+      <div className='shareOption'>
+        <h3>securely send files and more to nearby devices</h3>
+        <div className='shareButtons'>
+        <button type="button" className="btn btn-primary">Send</button>
+        <button type="button" className="btn btn-primary">Recieve</button>
+        </div>
+      </div>
+    </div>
     </>
   )
 }
